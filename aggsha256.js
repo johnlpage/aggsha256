7e3dcec6ae434767651f73f84fc4a64b8f26aa62
John Page <john.page@10gen.com>
2024-12-06 12:12:51 +0000

    Updated Comment

--- aggsha256.js
+++ aggsha256.js
@@ -2,8 +2,6 @@
 
 const { agg_sha256 } = require("./agg_hashfns.js");
 
-
-
 db = db.getSiblingDB("hasher")
 db.hasher.drop()
 
@@ -13,6 +11,9 @@ db.hasher.insertOne(document);
 
 console.log("Data Loaded");
 
+// Pipeline is multiple stages this the ..., whilst it "Could" be an expression this is just 
+// simpler to understand. It's also only hashing the first 448b block although it's pretty easy to 
+// extend to more blocks as seen in mongoaggminer 
 
 hashSteps = agg_sha256("$plainText", "hash_value")
 var cursor = db.hasher.aggregate([...hashSteps])
